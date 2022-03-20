02bca689955640d35ebf107276ac4ee80ef288b0
rajdada <rajgote1008@gmail.com>
2022-03-20 17:20:21 +0100

    minor updates in Customer Repo

--- src/main/java/com/shop/services/mycart/dao/CustomerRepository.java
+++ src/main/java/com/shop/services/mycart/dao/CustomerRepository.java
@@ -2,7 +2,12 @@ package com.shop.services.mycart.dao;
 
 import com.shop.services.mycart.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
 public interface CustomerRepository extends JpaRepository<Customer, Integer>
 {
 
